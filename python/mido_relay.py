a4f48b30c890d71ffdfa1d78950534b9bbe51331
Mark Wilson <mark@warkmilson.com>
2019-08-18 23:59:26 -0700

    synchronize before send; use floats for motion; tweak motion

--- python/mido_relay.py
+++ python/mido_relay.py
@@ -5,7 +5,7 @@ import mido
 import serial
 
 
-ARDUINO_BAUD_RATE = 560800
+ARDUINO_BAUD_RATE = 9600
 
 serials = os.listdir('/dev/serial/by-id/')
 print('\n'.join(serials) + '\n')
@@ -13,16 +13,33 @@ arduino_port = '/dev/serial/by-id/' + [s for s in serials if 'arduino' in s.lowe
 
 midi_inputs = mido.get_input_names()
 print('\n'.join(midi_inputs) + '\n')
-keyboard_port = [m for m in midi_inputs if 'Keystation' in m and m.endswith(':0')][0]
+keyboard_port = [
+    m for m in midi_inputs
+    if any(name in m for name in ('Keystation', 'Roland', 'Portable Grand'))
+    and m.endswith(':0')
+][0]
 
 # Note indices for 88-key keyboard go from 21 to 108
 key_on = defaultdict(bool)
 
-with mido.open_input('Keystation 88:Keystation 88 MIDI 1 20:0') as midi_in:
+notes = []
+
+with mido.open_input(keyboard_port) as midi_in:
     with serial.Serial(arduino_port, ARDUINO_BAUD_RATE) as arduino_serial:
-        for msg in midi_in:
-            if msg.type == 'note_on':
-                key_on[msg.note] = msg.velocity > 0
+        while True:
+            msg = midi_in.poll()
+            if msg:
+                if msg.type == 'note_on':
+                    key_on[msg.note] = msg.velocity > 0
+                    notes.extend([msg.note, 1 if msg.velocity > 0 else 0])
+                elif msg.type == 'note_off':
+                    key_on[msg.note] = False
+                    notes.extend([msg.note, 0])
+                print(msg)
+            if arduino_serial.in_waiting > 0:
+                bytes = arduino_serial.read(arduino_serial.in_waiting)
+                print(bytes)
+                print(notes)
+                arduino_serial.write([int(len(notes) / 2)] + notes)
                 arduino_serial.flush()
-                arduino_serial.write([msg.note, 1 if msg.velocity > 0 else 0])
-            print(msg)
+                notes = []
