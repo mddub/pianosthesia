0fb65b8a312636b8eb58bcf26166145c2afa3ac3
Mark Wilson <mark@warkmilson.com>
2019-08-18 23:59:26 -0700

    synchronize before send; use floats for motion; tweak motion

--- arduino/piano-lights/piano-lights.ino
+++ arduino/piano-lights/piano-lights.ino
@@ -21,12 +21,16 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 #define MIDDLE_LED 94
-#define NUM_RIPPLES 20
-#define FRAME_DELAY 50 // 20 fps
+#define NUM_RIPPLES 30
+#define FRAME_DELAY 20
+#define INITIAL_RIPPLE_TICKS 70
+#define INITIAL_RIPPLE_VELOCITY 1.6
+#define MINIMUM_RIPPLE_VELOCITY 0.05
+#define RIPPLE_DECELERATION 0.88
 
 static uint16_t ripple_start[NUM_RIPPLES];
-static uint16_t ripple_velocity[NUM_RIPPLES];
-static uint16_t ripple_distance[NUM_RIPPLES];
+static float ripple_velocity[NUM_RIPPLES];
+static float ripple_distance[NUM_RIPPLES];
 static uint8_t ripple_color[NUM_RIPPLES];
 static uint8_t ripple_ticks[NUM_RIPPLES];
 
@@ -68,17 +72,17 @@ void step_ripple() {
   strip.clear();
   for(uint8_t i = next_index; i != (next_index + NUM_RIPPLES - 1) % NUM_RIPPLES; i = (i + 1) % NUM_RIPPLES) {
     if (ripple_ticks[i] != 0) {
-      if (ripple_distance[i] == 0) { // first hit
+      if (ripple_ticks[i] == INITIAL_RIPPLE_TICKS) { // first hit
         strip.setPixelColor(ripple_start[i], Wheel(ripple_color[i], true));
       } else {
-        strip.setPixelColor(ripple_start[i] + ripple_distance[i], Wheel(ripple_color[i], false));
-        strip.setPixelColor(ripple_start[i] - ripple_distance[i], Wheel(ripple_color[i], false));
+        strip.setPixelColor((uint16_t)(ripple_start[i] + ripple_distance[i]), Wheel(ripple_color[i], false));
+        strip.setPixelColor((uint16_t)(ripple_start[i] - ripple_distance[i]), Wheel(ripple_color[i], false));
       }
 
       ripple_distance[i] = ripple_distance[i] + ripple_velocity[i];
-      ripple_velocity[i] = (float)ripple_velocity[i] * 0.99999;
-      if (ripple_velocity[i] < 1) {
-        ripple_velocity[i] = 1;
+      ripple_velocity[i] = ripple_velocity[i] * RIPPLE_DECELERATION;
+      if(ripple_velocity[i] < MINIMUM_RIPPLE_VELOCITY) {
+        ripple_velocity[i] = MINIMUM_RIPPLE_VELOCITY;
       }
       ripple_ticks[i]--;
     }
@@ -93,7 +97,7 @@ void step_ripple() {
 
 void setup() {
   last_frame = millis();
-  Serial.begin(560800);
+  Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   /*
@@ -104,37 +108,38 @@ void setup() {
   */
 }
 
-void reset_ripple(uint8_t i, uint16_t start_pos, uint16_t velocity, uint8_t color, uint8_t ticks_remaining) {
+void reset_ripple(uint8_t i, uint16_t start_pos,  uint8_t color) {
   ripple_start[i] = start_pos;
-  ripple_velocity[i] = velocity ;
+  ripple_velocity[i] = INITIAL_RIPPLE_VELOCITY;
   ripple_distance[i] = 0;
   ripple_color[i] = color;
-  ripple_ticks[i] = ticks_remaining;
+  ripple_ticks[i] = INITIAL_RIPPLE_TICKS;
 }
 
 void loop() {
   unsigned long now = millis();
 
-  while (Serial.available() > 0) {
-    char inChar = Serial.read();
-    // uint8_t start_led = (93 + (60 - inChar) * 1.5);
-    uint8_t start_led = (74 + (60 - inChar) * 1.5);
-    uint8_t color = (inChar % 12) * 21;
-
-    char on = Serial.read();
-
-    if (on) {
-      led_on[start_led] = true;
-      led_solid_color[start_led] = color;
-      uint16_t velocity = 6;
-      reset_ripple(next_index, start_led, velocity, color, 500 / velocity);
-      next_index = (next_index + 1) % NUM_RIPPLES;
-    } else {
-      led_on[start_led] = false;
-    }
-  }
   if ((now - last_frame) > FRAME_DELAY) {
     last_frame = now;
     step_ripple();
+    Serial.write(1);
+    while (!Serial.available()) {}
+    uint8_t num_pairs = Serial.read();
+    for(uint8_t i = 0; i < num_pairs; i++) {
+      char inChar = Serial.read();
+      char on = Serial.read();
+
+      uint8_t start_led = (74 + (60 - inChar) * 1.5);
+      uint8_t color = (inChar % 12) * 21;
+
+      if (on) {
+        led_on[start_led] = true;
+        led_solid_color[start_led] = color;
+        reset_ripple(next_index, start_led, color);
+        next_index = (next_index + 1) % NUM_RIPPLES;
+      } else {
+        led_on[start_led] = false;
+      }
+    }
   }
 }
